aedb2dcf2de3b8580a56b09201c2440e80977389
riverpeek <rapeek@ucdavis.edu>
2016-04-11 15:41:24 -0700

    Update with shiny demo and script to merge weixelman data

--- R/map_weixelman_meadows.R
+++ R/map_weixelman_meadows.R
@@ -5,5 +5,19 @@
 
 library(dplyr); library(readr); library(lubridate)
 
+# READ IN DATA ------------------------------------------------------------
 
-dat<-read_csv("./data/")
+## Weixelman Data
+dat<-read_csv("./data/base/2016 Sierra Nevada FS plots March 29 2016.csv")
+str(dat)
+
+load("./data/base/mdw_geedat2.rda")  # mdws: full dataset
+str(mdws)
+
+load("./data/base/mdw_xydat.rda") # xydat: mdws dataset just XYs # but not full xy set? WTF?
+str(xydat)
+
+# MERGE WITH EXISTING MEADOWS ---------------------------------------------
+
+wx.mdw<-left_join(dat, xydat, by=c("UCDavis_Object_ID"="ID"))
+wx.mdw
