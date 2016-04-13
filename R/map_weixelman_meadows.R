67558bc6d684f88d664e5690497675456e16609c
riverpeek <rapeek@ucdavis.edu>
2016-04-12 17:59:15 -0700

    update with shpfile

--- R/map_weixelman_meadows.R
+++ R/map_weixelman_meadows.R
@@ -11,10 +11,17 @@ library(dplyr); library(readr); library(lubridate)
 dat<-read_csv("./data/base/2016 Sierra Nevada FS plots March 29 2016.csv")
 str(dat)
 
-load("./data/base/mdw_geedat2.rda")  # mdws: full dataset
-str(mdws)
+load("./data/base/mdw_geedat2.rda")  # mdws: full google earth dataset (1424 mdws)
+# str(mdws)
 
-load("./data/base/mdw_xydat.rda") # xydat: mdws dataset just XYs # but not full xy set? WTF?
+# need to extract the XY for meadows to plot:
+mdws_xy<- comb %>% distinct(ID) %>%
+  #filter(!is.na(lat)) %>%
+  as.data.frame()
+
+# only meadows over 1.4 hectares here
+load("./data/base/mdw_xydat.rda") # xydat: GE mdws dataset just XYs  
+
 str(xydat)
 
 # MERGE WITH EXISTING MEADOWS ---------------------------------------------
